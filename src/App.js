ca7d0c5c70ec9ac83890f46d12d0de1f772ae8fa
rezaBG <rezabarzegargashti@gmail.com>
2023-04-12 21:40:04 +0200

    update 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import ProductList from "./components/ProductList/ProductList";
 import AddProduct from "./components/AddProduct/AddProduct";
+import { async } from "q";
 
 const App = () => {
-  const [products, setProduct] = useState([
-    { id: 1, title: "Book 1" },
-    { id: 2, title: "Book 2" },
-    { id: 3, title: "Book 3" },
-  ]);
+  const [products, setProduct] = useState([]);
 
-  const deleteProduct = (id) => {
+  useEffect(() => {
+    const sendRequest = async () => {
+      const response = await fetch("http://localhost:8000/product");
+
+      const responseData = await response.json();
+
+      console.log(responseData);
+    };
+
+    setProduct(responseData);
+  }, []);
+
+  const AddProduct = async (title) => {
+    const responce = await fetch("http://localhost:8000/product", {
+      method: "POST",
+      headers: {
+        "Content-type": "aplication/json",
+      },
+      body: JSON.stringify(title),
+    });
+
+    const responseData = await responce.JSON();
+
+    setProduct([...products, responseData]);
+  };
+
+  const deleteProduct = async (id) => {
+    await fetch(`http://localhost:8000/product/${id}`, {
+      method: "DELETE",
+    });
+
     setProduct(products.filter((item) => item.id !== id));
   };
 
   return (
     <div className="container">
-      <AddProduct />
+      <AddProduct onAdd={AddProduct} />
       <ProductList products={products} onDelete={deleteProduct} />
     </div>
   );
